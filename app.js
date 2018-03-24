f45343cf9e903626eb41074f94ad90f166af4db0
Stand <30985599+standnguyen197@users.noreply.github.com>
2018-03-24 10:13:10 +0700

    Up 2

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use('/books', express.static(path.join(__dirname, 'dist')));
 app.use('/book', book);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
