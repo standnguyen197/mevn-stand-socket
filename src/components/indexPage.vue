c7ca90ec3dd6ceab25695fcfad9f79cff437a543
Stand <30985599+standnguyen197@users.noreply.github.com>
2018-03-24 10:08:49 +0700

    Up 1

---
+++ src/components/indexPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-row>
+    <b-btn @click="clickOn">Click</b-btn>
+    {{ testMessage }}
+  </b-row>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'indexPage',
+  data () {
+    return {
+    testMessage: 'Ahihi'
+    }
+  },
+  sockets:{
+    connect: function(){
+      console.log('socket connected')
+    },
+    customEmit: function(val){
+      console.log(`this method was fired by the socket server. ${val.hello}`)
+    }
+  },
+  methods: {
+      clickOn(){
+        this.testMessage = 'testMessage!'
+      }
+  }
+}
+</script>
